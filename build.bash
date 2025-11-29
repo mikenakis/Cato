b1939e2ddf12c3e5df11ddafe5505e5db15ec3e3
MikeNakis <git@michael.gr>
2025-11-29 14:31:47 +0100

    redoing the way building is done

--- build.bash
+++ build.bash
@@ -1,192 +1,227 @@
 #!/bin/bash
 
-set -e # magical incantation to immediately exit if any command has a non-zero exit status. PEARL: it still won't fail if any of the following `set` commands fails.
-set -u # magical incantation to mmediately exit if an undefined variable is referenced.
-set -o pipefail # magical incantation to prevent pipelines from masking errors. (Use `command1 | command2 || true` to mask.)
-shopt -s extglob # magical incantation to enable extended pattern matching.
+source definitions.bash > /dev/null
 
-# set -x # magical incantation to enable echoing commands for the purpose of troubleshooting.
+declare -r version_file_pathname=$(realpath version.txt)
 
-function increment_version()
+function do_restore()
 {
-	local part_to_increment=$1
-	local version=$2
-	local major
-	local minor
-	local patch
-
-	IFS=. read -r major minor patch <<< "$version"
-	case "$part_to_increment" in
-		"increment_major")
-			major=$((major+1))
-			minor=0
-			patch=0
-			;;
-		"increment_minor")
-			minor=$((minor+1))
-			patch=0
-			;;
-		"increment_patch")
-			patch=$((patch+1))
-			;;
-		*)
-			printf "%s: Invalid argument: '%s'\n" "$0" "$1"
-			return 1 # does this cause the script to fail?
-	esac
-	printf "%s.%s.%s" "$major" "$minor" "$patch"
+	info "Restore"
+	[[ $dry_run == true ]] && return
+	dotnet restore -check --verbosity minimal
 }
 
-function create_next_version()
+function do_build()
 {
-	local part_to_increment=$1
-	local version=$2
-
-	local next_version=$(increment_version "$part_to_increment" "$version")
-	printf "old version: %s new version: %s\n" "$version" "$next_version"
-	printf "%s" "$next_version" > version.txt
-	git add version.txt
-	git commit --message="increment version from $version to $next_version"
+	declare -r configuration=$1
+	info "Build '$1' configuration"
+	[[ $dry_run == true ]] && return
+	dotnet build -check --verbosity minimal --configuration "$configuration" --no-restore
 }
 
-function write_and_commit_version_file()
+function do_test()
 {
-	local version=$1
-	local next_version=$2
-
-	printf "old version: %s new version: %s\n" "$version" "$next_version"
-	printf "%s" "$next_version" > version.txt
-	git add version.txt
-	git commit --message="increment version from $version to $next_version"
+	declare -r configuration=$1
+	info "Test '$1' configuration"
+	[[ $dry_run == true ]] && return
+	dotnet test -check --verbosity normal --configuration "$configuration" --no-build
 }
 
-function assert_no_untracked_files()
+function do_publish()
 {
-	local untracked_files=$(git ls-files -o --directory --exclude-standard --no-empty-directory)
-	if [ "$untracked_files" != "" ]; then
-		echo "You have untracked files:"
-		echo $untracked_files
-		echo "Please add, stage, and commit first."
-		return 1
+	declare -r command=$1
+	declare -r configuration=$2
+	declare -r project_name=$3
+	declare -r github_packages_nuget_api_key=$4
+	declare -r nuget_org_nuget_api_key=$5
+
+	declare -r package_pathname=$project_name/bin/$configuration/*.nupkg
+
+	info "Publish '$configuration' configuration (project '$project_name')"
+	[[ $dry_run == true ]] && return
+
+	# PEARL: dotnet nuget push will push a package specified using a wildcard, but if the wildcard matches more than one file, then it
+	#    will sabotage the developer by failing with a misleading error message that says "File does not exist" instead of "More than
+	#    one file exists".  For this reason, we have to remove all files matching the wildcard before creating the package.
+	remove_if_exists "$package_pathname"
+	dotnet pack -check --verbosity normal --configuration "$configuration" --no-build
+	
+	if [[ "$command" == "auto" ]]; then
+		dotnet nuget push "$package_pathname" --source https://nuget.pkg.github.com/MikeNakis/index.json --api-key "$github_packages_nuget_api_key"
+	else
+		dotnet nuget push "$package_pathname" --source https://api.nuget.org/v3/index.json --api-key "$nuget_org_nuget_api_key"
 	fi
 }
 
-function assert_no_tracked_but_unstaged_changes()
+function run()
 {
-	local unstaged_files=$(git diff-files --name-only)
-	if [ "$unstaged_files" != "" ]; then
-		echo "You have tracked but unstanged changes:"
-		echo $unstaged_files
-		echo "Please stage and commit first."
-		return 1
-	fi
-}
+	declare -l command=""
+	declare project_name=""
+	declare github_packages_nuget_api_key=""
+	declare nuget_org_nuget_api_key=""
+	declare dry_run=false
 
-function assert_no_staged_but_uncommitted_changes()
-{
-	local uncommitted_files=$(git diff-index --name-only --cached HEAD)
-	if [ "$uncommitted_files" != "" ]; then
-		echo "You have staged but uncommitted changes:"
-		echo $uncommitted_files
-		echo "Please unstage or commit first."
-		return 1
-	fi
-}
+	while [ $# -gt 0 ]; do
+		case "$1" in
+			--command=*)
+				command="${1#*=}"
+				;;
+			--project-name=*)
+				project_name="${1#*=}"
+				;;
+			--github-packages-nuget-api-key=*)
+				github_packages_nuget_api_key="${1#*=}"
+				;;
+			--nuget-org-nuget-api-key=*)
+				nuget_org_nuget_api_key="${1#*=}"
+				;;
+			--dry-run)
+				dry_run=true
+				;;
+			*)
+				error "Invalid argument: '%s'" "$1"
+				exit 1
+		esac
+		shift
+	done
 
-function remove_quietly()
-{
-	local file=$1
-	if [ -f "$file" ]; then
-		rm "$file"
-	fi
-}
-
-function command_auto_output_type_tool()
-{
-	dotnet test -check --configuration Debug --verbosity minimal
-	remove_quietly ${project_name}/bin/Release/*.nupkg
-	dotnet pack -check --configuration Release --property:PublicRelease=true
-	dotnet nuget push ${project_name}/bin/Release/*.nupkg --source https://nuget.pkg.github.com/MikeNakis/index.json --api-key ${github_packages_nuget_api_key}
-}
-
-function command_manual_output_type_tool()
-{
-	remove_quietly ${project_name}/bin/Release/*.nupkg
-	dotnet pack -check --configuration Release --property:PublicRelease=true
-	dotnet nuget push ${project_name}/bin/Release/*.nupkg --source https://api.nuget.org/v3/index.json --api-key ${nuget_org_nuget_api_key}
-}
-
-function command_auto()
-{
-	assert_no_staged_but_uncommitted_changes
-
-	case "$output_type" in
-		"tool")
-			command_auto_output_type_tool
-			;;
-		*)
-			printf "%s: Invalid argument: '%s'\n" "$0" "$1"
-			exit 1
-	esac
-
-	local version=$(cat version.txt)
-	git tag "$version"
-	create_next_version increment_patch $version
-	printf "Pushing...\n"
-	git push origin HEAD --tags
-}
-
-function command_manual()
-{
-	assert_no_staged_but_uncommitted_changes
-
-	case "$output_type" in
-		"tool")
-			command_manual_output_type_tool
-			;;
-		*)
-			printf "%s: Invalid argument: '%s'\n" "$0" "$1"
-			exit 1
-	esac
-
-	local version=$(cat version.txt)
-	git tag "$version"
-	create_next_version increment_patch $version
-	printf "Pushing...\n"
-	git push origin HEAD --tags
-}
-
-while [ $# -gt 0 ]; do
-	case "$1" in
-		Command=*)
-			command="${1#*=}"
-			;;
-		OutputType=*)
-			output_type="${1#*=}"
-			;;
-		ProjectName=*)
-			project_name="${1#*=}"
-			;;
-		GitHubPackagesNuGetApiKey=*)
-			github_packages_nuget_api_key="${1#*=}"
-			;;
-		NuGetOrgNuGetApiKey=*)
-			nuget_org_nuget_api_key="${1#*=}"
-			;;
-		*)
-			printf "%s: Invalid argument: '%s'\n" "$0" "$1"
-			exit 1
-	esac
-	shift
-done
-
-case "$command" in
-	"auto")
-		command_auto
-		;;
-	"manual")
-		command_manual
-		;;
-	*)
-		printf "%s: Invalid argument: '%s'\n" "$0" "$command"
+	if [ -z "$command" ]; then
+		error "Missing argument: '%s'" "Command"
 		exit 1
-esac
+	fi
+
+	if [ -z "$project_name" ]; then
+		error "Missing argument: '%s'" "ProjectName"
+		exit 1
+	fi
+
+	if [ -z "$github_packages_nuget_api_key" ]; then
+		error "Missing argument: '%s'" "GitHubPackagesNuGetApiKey"
+		exit 1
+	fi
+
+	if [ -z "$nuget_org_nuget_api_key" ]; then
+		error "Missing argument: '%s'" "NuGetOrgNuGetApiKey"
+		exit 1
+	fi
+
+	case "$command" in
+		"auto")
+			;;
+		"manual")
+			;;
+		*)
+			error "Unknown command: '%s'" "$command"
+			exit 1
+	esac
+
+	if [ ! -f *.sln? ]; then
+		error "Current directory is not a solution directory."
+	fi
+
+	declare -r project_file=$project_name/$project_name.csproj
+	if [ ! -f "$project_file" ]; then
+		error "Project file not found: %s" "$project_file"
+	fi
+
+	declare -r -l output_type=$(get_output_type "$project_file")
+	
+	declare -r -l pack_as_tool=$(get_pack_as_tool "$project_file")
+
+	declare -r configurations=$(get_configurations "$project_file")
+
+	if [[ "$output_type" == "exe" && $configurations == *Develop* ]]; then
+		warn "This project builds an executable but has a 'Develop' configuration!?"
+	fi
+	
+	# The logic:
+	#  First, do a dotnet restore.
+	#  Then, build and test the 'Debug' configuration, or the 'Optimized' configuration if one has been defined.
+	#  Then, if this is a library, or an exe packaged as tool, build and publish the 'Develop' and 'Release'
+	#  configurations, each if defined.
+
+	do_restore
+
+	if [[ "$configurations" == *Optimized* ]]; then
+		do_build Optimized
+		do_test Optimized
+	elif [[ "$configurations" == *Debug* ]]; then
+		do_build Debug
+		do_test Debug
+	fi
+
+	if [[ "$output_type" == "library" || "$output_type" == "exe" && "$pack_as_tool" == "true" ]]; then
+
+		if [[ "$configurations" == *Develop* ]]; then
+			do_build Develop
+			do_publish "$command" Develop "$project_name" "$github_packages_nuget_api_key" "$nuget_org_nuget_api_key"
+		fi
+
+		if [[ "$configurations" == *Release* ]]; then
+			do_build Release
+			do_publish "$command" Release "$project_name" "$github_packages_nuget_api_key" "$nuget_org_nuget_api_key"
+		fi
+
+	fi
+}
+
+function get_output_type
+{
+	declare -r project_file=$1
+
+	# PEARL: In an MSBuild project file, a missing OutputType property defaults to 'Library'; however, a present but
+	#    empty OutputType property seems to default to 'Exe'! (WTF?) Note: we are not accounting for a present but empty
+	#    OutputType property here.
+	declare -l output_type=$(get_xml_value "$project_file" OutputType)
+
+	if [[ "$output_type" != "library" && "$output_type" != "exe" && "$output_type" != "" ]]; then
+		error "Invalid output type: '%s'" "$output_type"
+		exit 1
+	fi
+
+	if [ -z "$output_type" ]; then
+		output_type="library"
+	fi
+
+	printf "%s" "$output_type"
+}
+
+function get_configurations
+{
+	declare -r project_file=$1
+
+	declare -r configurations=$(get_xml_value "$project_file" Configurations)
+
+	declare t="$configurations"
+	t=${t/Debug}
+	t=${t/Optimized}
+	t=${t/Develop}
+	t=${t/Release}
+	t=${t//;}
+	if [[ "$t" != "" ]]; then
+		error "Invalid configuration: '%s'." "$t"
+		exit 1
+	fi
+
+	printf "%s" "$configurations"
+}
+
+function get_pack_as_tool
+{
+	declare -r project_file=$1
+
+	declare -l pack_as_tool=$(get_xml_value "$project_file" PackAsTool)
+
+	if [[ "$pack_as_tool" != "" && "$pack_as_tool" != "true" && "$pack_as_tool" != "false" ]]; then
+		error "Invalid 'PackAsTool' value: '%s'" "$pack_as_tool"
+		exit 1
+	fi
+
+	if [ -z "$pack_as_tool" ]; then
+		pack_as_tool="false"
+	fi
+
+	printf "%s" "$pack_as_tool"
+}
+
+run $@
